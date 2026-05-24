d713e2898b3b6b72a705035133932ffe8e4a661e
abd <abdkadour20@gmail.com>
2026-05-24 17:30:46 +0300

    fix service worker manifest

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,4 @@
 /* eslint-disable no-restricted-globals */
-import { clientsClaim } from 'workbox-core';
-import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
+import { precacheAndRoute } from 'workbox-precaching';
 
-clientsClaim();
-
-precacheAndRoute(self.__WB_MANIFEST || []);
-cleanupOutdatedCaches();
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+precacheAndRoute(self.__WB_MANIFEST);
